cc16463c6cb7840eb21638da717949cd1bed75d9
Kasun-se-2018-023 <skasunmk982@gmail.com>
2021-09-19 22:30:00 +0530

    add exception for DivOperation to v5 Unit Testing

--- src/java/com/kelani/v5/operation/DivOperation.java
+++ src/java/com/kelani/v5/operation/DivOperation.java
@@ -1,7 +1,12 @@
 package com.kelani.v5.operation;
 
+
+
 public class DivOperation implements Operation {
-    public Double execute(Double[] numbers) {
+    public Double execute(Double[] numbers) throws InvalidCalcOperationException {
+        if(numbers[1]==0){
+            throw new InvalidCalcOperationException("Do not divide by zero");
+        }
         return numbers[0] / numbers[1];
     }
 }
