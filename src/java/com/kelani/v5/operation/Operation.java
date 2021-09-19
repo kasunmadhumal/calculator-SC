30c054e8fa31d26068aa2ee095106f5dd4a9a449
Kasun-se-2018-023 <skasunmk982@gmail.com>
2021-09-19 22:34:03 +0530

    add throw exeption

--- src/java/com/kelani/v5/operation/Operation.java
+++ src/java/com/kelani/v5/operation/Operation.java
@@ -1,5 +1,5 @@
 package com.kelani.v5.operation;
 
 public interface Operation {
-    Double execute(Double[] numbers);
+    Double execute(Double[] numbers) throws InvalidCalcOperationException, InvalidCalcOperationException;
 }
